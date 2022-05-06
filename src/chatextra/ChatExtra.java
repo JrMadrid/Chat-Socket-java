ff6927407f1b1eff47dc95304d2669ce5cf52014
Juan Madrid <72727166+JrMadrid@users.noreply.github.com>
2022-05-06 00:09:37 -0600

    subiendo original chat socket java de POO

---
+++ src/chatextra/ChatExtra.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatextra;
+
+/**
+ *
+ * @author JuanRMadrid
+ */
+public class ChatExtra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frm1 cliente = new Frm1();
+        cliente.setVisible(true);
+        
+        Frm2 servidor = new Frm2();
+        servidor.setVisible(true);
+    }
+    
+}
---
+++ src/chatextra/Cliente.java
@@ -0,0 +1,44 @@
+package chatextra;
+
+ 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+ 
+public class Cliente implements Runnable {
+ 
+    private int puerto;
+    private String mensaje;
+ 
+    public Cliente(int puerto, String mensaje) {
+        this.puerto = puerto;
+        this.mensaje = mensaje;
+    }
+ 
+    @Override
+    public void run() {
+        //Host del servidor
+        final String HOST = "192.168.0.16"; //PONER LA OP QUE TENES EN TU COMPUTADOR (WINDOWS-CMD: IPCONFIG)
+        //Puerto del servidor
+        DataOutputStream out;
+ 
+        try {
+            //Creo el socket para conectarme con el cliente
+            Socket sc = new Socket(HOST, puerto);
+ 
+            out = new DataOutputStream(sc.getOutputStream());
+ 
+            //Envio un mensaje al cliente
+            out.writeUTF(mensaje);
+ 
+            sc.close();
+ 
+        } catch (IOException ex) {
+            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ 
+    }
+}
